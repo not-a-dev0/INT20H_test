72c291972b3bc350d74fa1e7cea33e3dda379852
Olena Galitska <olenagalitska@Olenas-MacBook-Pro.local>
2019-02-08 17:03:14 +0200

    made a set of photo links

--- app/flickr/flickr_api.py
+++ app/flickr/flickr_api.py
@@ -1,8 +1,4 @@
-import io
-
-from flask import jsonify, send_file
 import requests
-import json
 
 import xmltodict as xmltodict
 
@@ -11,6 +7,7 @@ class FlickrPhotosGetter:
 
     @staticmethod
     def get_photos():
+
         album_owner = "144522605@N06"
         album_id = "72157674388093532"
 
@@ -39,53 +36,36 @@ class FlickrPhotosGetter:
 
         # response = requests.post(request_url, args)
 
+        all_photos = set()
+
         response_album_photos = requests.post(request_url_get_album_photos, args)
 
         if response_album_photos.status_code == 200:
-            # json_response_album_photos = json.loads(response_album_photos.text)
             json_response_album_photos = response_album_photos.json()
-            print("\nalbum photos:")
             photos = json_response_album_photos["photoset"]["photo"]
 
-            photo = photos[0]
             for photo in photos:
-                print(photo)
-                request_url_get_photo = "http://farm" + str(photo["farm"]) + ".staticflickr.com/" + str(
+                photo_link = "http://farm" + str(photo["farm"]) + ".staticflickr.com/" + str(
                     photo["server"]) + "/" + \
-                                        str(photo["id"]) + "_" + str(photo["secret"]) + ".jpg"
+                             str(photo["id"]) + "_" + str(photo["secret"]) + ".jpg"
+                # print(photo_link)
+                all_photos.add(photo_link)
 
-                response_photo = requests.post(request_url_get_photo, args)
-                if response_photo.status_code == 200:
-                    photo_file = send_file(
-                        io.BytesIO(response_photo.content),
-                        mimetype='image/jpeg')
-                    return photo_file
+        response_hashtag_photos = requests.post(request_url_get_hashtag_photos, args)
 
-                # response_hashtag_photos = requests.post(request_url_get_hashtag_photos, args)
-        #
-        # if response_hashtag_photos.status_code == 200:
-        #     # json_response_hashtag_photos = jsonify(xmltodict.parse())
-        #     # print(json_response_hashtag_photos)
-        #
-        #     print("\n\nhashtag photos:")
-        #     # print(response_hashtag_photos) - it's root object, print "<Response [200]>"
-        #     # response_hashtag_photos.raw - return address of an object
-        #     # response_hashtag_photos.json() - error
-        #
-        #     print("\n\ntext")
-        #     print(response_hashtag_photos.text)
-        #
-        #     print("\n\ncontent")
-        #     print(response_hashtag_photos.content)
-        #
-        #     # xmltodict.parse(response_hashtag_photos.content) same as xmltodict.parse(response_hashtag_photos.text)
-        #
-        #     xmldict_from_text = xmltodict.parse(response_hashtag_photos.text)
-        #     print("\n\nxml dict from .text")
-        #     # print(xmldict_from_text)
-        #
-        #     # xmldict_from_content = xmltodict.parse(response_hashtag_photos.content)
-        #     # print("\n\nxml dict from .content")
-        #     # print(xmldict_from_content)
-        #
-        #     print(xmldict_from_text["rsp"]["photos"])
+        if response_hashtag_photos.status_code == 200:
+
+            xmldict_from_text = xmltodict.parse(response_hashtag_photos.text)
+
+            print(xmldict_from_text["rsp"]["photos"])
+            # number_of_pages = xmldict_from_text["rsp"]["photos"]["@pages"]
+            hashtag_photos = xmldict_from_text["rsp"]["photos"]["photo"]
+            for photo in hashtag_photos:
+                photo_link = "http://farm" + str(photo["@farm"]) + ".staticflickr.com/" + str(
+                    photo["@server"]) + "/" + \
+                             str(photo["@id"]) + "_" + str(photo["@secret"]) + ".jpg"
+                # print(photo_link)
+                all_photos.add(photo_link)
+
+        print(all_photos)
+        return all_photos
--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,5 @@ from app.flickr.flickr_api import FlickrPhotosGetter
 
 @app.route('/')
 def hello_world():
-    return FlickrPhotosGetter.get_photos()
-    # return 'Hello World!'
+    FlickrPhotosGetter.get_photos()
+    return 'Hello World!'
