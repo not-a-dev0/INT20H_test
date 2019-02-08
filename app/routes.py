6d2eade54892e1edafeee4b473ad2c5fd8ae8787
dimabranik <dmitriybranitskiy@gmail.com>
2019-02-09 00:59:29 +0200

    Fixed app/routes

--- app/routes.py
+++ app/routes.py
@@ -4,16 +4,9 @@ from app import app
 from app.flickr.flickr_api import FlickrPhotosGetter
 from app.faceplusplus.faceplusplus_api import FacePlusPlusApi
 
-# from app.dbmanager.photos_info_manager import photos_info_db_manager
-
 
 @app.route('/')
 def hello_world():
-    # return render_template('base.html', data=data)
-
-    photo_urls = FlickrPhotosGetter.get_photo_urls()
-    FacePlusPlusApi.detect_faces(photo_urls)
-
     return render_template('base.html')
 
 
