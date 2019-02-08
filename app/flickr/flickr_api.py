75f5798c6c55bdcccf5bfd741c483cec05fdcecc
Olena Galitska <olenagalitska@Olenas-MacBook-Pro.local>
2019-02-08 16:18:38 +0200

    got a photo from response

--- app/flickr/flickr_api.py
+++ app/flickr/flickr_api.py
@@ -1,4 +1,6 @@
-from flask import jsonify
+import io
+
+from flask import jsonify, send_file
 import requests
 import json
 
@@ -9,14 +11,15 @@ class FlickrPhotosGetter:
 
     @staticmethod
     def get_photos():
+        album_owner = "144522605@N06"
+        album_id = "72157674388093532"
 
-        request_url_get_album_photos = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=8ff7a9ee623f29351d684a2b6c7e4e14&format=json&nojsoncallback=1&photoset_id=72157674388093532&user_id=144522605@N06"
+        request_url_get_album_photos = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=8ff7a9ee623f29351d684a2b6c7e4e14&format=json&nojsoncallback=1&photoset_id=" + album_id
+        "&user_id=" + album_owner
 
-        request_url_get_hashtag_photos = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8ff7a9ee623f29351d684a2b6c7e4e14&tags=int20h" #nature - for testing result parsing on more than 100 results
+        request_url_get_hashtag_photos = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8ff7a9ee623f29351d684a2b6c7e4e14&tags=int20h"  # nature - for testing result parsing on more than 100 results
 
         # should use paggination, because default perpage = 100, and results for hashtag searching could be more than that
-
-
 
         api_key = "8ff7a9ee623f29351d684a2b6c7e4e14"
 
@@ -36,43 +39,53 @@ class FlickrPhotosGetter:
 
         # response = requests.post(request_url, args)
 
-
-
-
         response_album_photos = requests.post(request_url_get_album_photos, args)
 
         if response_album_photos.status_code == 200:
             # json_response_album_photos = json.loads(response_album_photos.text)
             json_response_album_photos = response_album_photos.json()
             print("\nalbum photos:")
-            print(json_response_album_photos)
+            photos = json_response_album_photos["photoset"]["photo"]
 
+            photo = photos[0]
+            for photo in photos:
+                print(photo)
+                request_url_get_photo = "http://farm" + str(photo["farm"]) + ".staticflickr.com/" + str(
+                    photo["server"]) + "/" + \
+                                        str(photo["id"]) + "_" + str(photo["secret"]) + ".jpg"
 
-        response_hashtag_photos = requests.post(request_url_get_hashtag_photos, args)
+                response_photo = requests.post(request_url_get_photo, args)
+                if response_photo.status_code == 200:
+                    photo_file = send_file(
+                        io.BytesIO(response_photo.content),
+                        mimetype='image/jpeg')
+                    return photo_file
 
-        if response_hashtag_photos.status_code == 200:
-            # json_response_hashtag_photos = jsonify(xmltodict.parse())
-            # print(json_response_hashtag_photos)
-
-            print("\n\nhashtag photos:")
-            # print(response_hashtag_photos) - it's root object, print "<Response [200]>"
-            # response_hashtag_photos.raw - return address of an object
-            # response_hashtag_photos.json() - error
-
-            print("\n\ntext")
-            print(response_hashtag_photos.text)
-
-            print("\n\ncontent")
-            print(response_hashtag_photos.content)
-
-            # xmltodict.parse(response_hashtag_photos.content) same as xmltodict.parse(response_hashtag_photos.text)
-
-            xmldict_from_text = xmltodict.parse(response_hashtag_photos.text)
-            print("\n\nxml dict from .text")
-            # print(xmldict_from_text)
-
-            # xmldict_from_content = xmltodict.parse(response_hashtag_photos.content)
-            # print("\n\nxml dict from .content")
-            # print(xmldict_from_content)
-
-            print(xmldict_from_text["rsp"]["photos"])
+                # response_hashtag_photos = requests.post(request_url_get_hashtag_photos, args)
+        #
+        # if response_hashtag_photos.status_code == 200:
+        #     # json_response_hashtag_photos = jsonify(xmltodict.parse())
+        #     # print(json_response_hashtag_photos)
+        #
+        #     print("\n\nhashtag photos:")
+        #     # print(response_hashtag_photos) - it's root object, print "<Response [200]>"
+        #     # response_hashtag_photos.raw - return address of an object
+        #     # response_hashtag_photos.json() - error
+        #
+        #     print("\n\ntext")
+        #     print(response_hashtag_photos.text)
+        #
+        #     print("\n\ncontent")
+        #     print(response_hashtag_photos.content)
+        #
+        #     # xmltodict.parse(response_hashtag_photos.content) same as xmltodict.parse(response_hashtag_photos.text)
+        #
+        #     xmldict_from_text = xmltodict.parse(response_hashtag_photos.text)
+        #     print("\n\nxml dict from .text")
+        #     # print(xmldict_from_text)
+        #
+        #     # xmldict_from_content = xmltodict.parse(response_hashtag_photos.content)
+        #     # print("\n\nxml dict from .content")
+        #     # print(xmldict_from_content)
+        #
+        #     print(xmldict_from_text["rsp"]["photos"])
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app
 from app.flickr.flickr_api import FlickrPhotosGetter
 
+
 @app.route('/')
 def hello_world():
-    FlickrPhotosGetter.get_photos()
-    return 'Hello World!'
+    return FlickrPhotosGetter.get_photos()
+    # return 'Hello World!'
