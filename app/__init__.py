b990723b5e7ab417157aa48d2c2155f40245c7d4
dimabranik <dmitriybranitskiy@gmail.com>
2019-02-09 17:01:24 +0200

    Added logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
+
+import logging.config
+import yaml
+from os import path, makedirs
+
 from app.threads.DB_updater import DBUpdater
 from app.conf.config import Config
 
@@ -6,9 +11,42 @@ try:
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    if not path.exists('logs'):
+        makedirs('logs')
+        logs_file = open('logs/logs.log', 'x')
+        logs_file.close()
+
+    if not path.exists('logs/logs.log'):
+        logs_file = open('logs/logs.log', 'x')
+        logs_file.close()
+
+    with open('app/conf/logging.yml', 'r') as stream:
+        logging_config = yaml.load(stream)
+    logging.config.dictConfig(logging_config)
+
+    from app.dbmanager.logs_manager import ArangoLogHandler
+    app.logger.addHandler(ArangoLogHandler())
+
+    logger = logging.getLogger('logger')
+
+    # example of using logger
+    # logger.debug('debug message')
+    # logger.info('info message')
+    # logger.warning('warning message')
+    # logger.error('error message')
+    # logger.critical('critical message')
+
+    # example of using logger with exceptions
+    # try:
+    #     open('/path/to/does/not/exist', 'rb')
+    # except (SystemExit, KeyboardInterrupt):
+    #     raise
+    # except Exception as e:
+    #     logger.error('Failed to open file', exc_info=True)
+
     from app import routes
 
-    db_updater = DBUpdater("Database Updater")
+    db_updater = DBUpdater('Database Updater')
     db_updater.start()
 
     if __name__ == '__main__':
---
+++ app/dbmanager/logs_manager.py
@@ -0,0 +1,62 @@
+import logging.handlers
+
+import datetime
+
+from arango import ArangoClient
+
+
+# def singleton(class_):
+#     instances = {}
+#
+#     def getinstance(*args, **kwargs):
+#         if class_ not in instances:
+#             instances[class_] = class_(*args, **kwargs)
+#         return instances[class_]
+#
+#     return getinstance
+
+class Singleton(object):
+    _instance = None
+
+    def __new__(class_, *args, **kwargs):
+        if not isinstance(class_._instance, class_):
+            class_._instance = object.__new__(class_, *args, **kwargs)
+        return class_._instance
+
+
+class ArangoLogHandler(Singleton, logging.Handler):
+    """
+    Logging handler for ArangoDB.
+
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.arangodb_client = ArangoClient(protocol='http', host='localhost', port=8529)
+        self.arangodb = self.arangodb_client.db('int20h_test', username='int20h_test_user', password='int20h')
+
+        try:
+            self.arangodb.create_collection('logs')
+        except:
+            pass
+
+        self.logs_collection = self.arangodb.collection('logs')
+
+    def emit(self, record):
+        msg = str(record.__dict__['msg'])
+        if len(record.__dict__['args']) > 0:
+            msg += '; args: ' + str(record.__dict__['args'])
+        if record.__dict__['exc_info'] is not None:
+            msg += str(record.__dict__['exc_info']) + str(record.__dict__['exc_text']) + str(
+                record.__dict__['stack_info'])
+
+        log = {
+            '_key': str(datetime.datetime.now()).replace(' ', '_'),
+            'pathname': record.__dict__['pathname'],
+            'level': record.__dict__['levelname'],
+            'func_name': record.__dict__['funcName'],
+            'line_no': record.__dict__['lineno'],
+            'msg': msg
+        }
+
+        self.logs_collection.insert(log)
